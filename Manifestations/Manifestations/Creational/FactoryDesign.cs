b3ddcdaa399b2cb4234bdf90166ee9fef11f5de5
Hemant Patidar <patidarhemant27@gmail.com>
2023-06-03 21:10:08 +0530

    Factory Design sample

---
+++ Manifestations/Manifestations/Creational/FactoryDesign.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manifestations.Creational
+{
+    internal class FactoryDesign
+    {
+        public static void main(string[] args)
+        {
+            ShapeInstanceFactory factoryObj = new ShapeInstanceFactory();
+            IShape? circleObject = factoryObj.getShapeInstance("Circle");
+            circleObject?.computeArea();
+        }
+    }
+
+    public interface IShape
+    {
+        void computeArea();
+    }
+
+    public class Square : IShape
+    {
+       public void computeArea()
+        {
+            Console.WriteLine("Compute Square Area");
+        }
+    }
+    public class Circle : IShape 
+    {
+        public void computeArea()
+        {
+            Console.WriteLine("Compute Circle Area");
+        }
+    }
+
+    public class ShapeInstanceFactory
+    {
+        public IShape? getShapeInstance(string value)
+        {
+            if (value.Equals("Squre"))
+            {
+                return new Square();
+            }
+            else if (value.Equals("Circle"))
+            {
+                return new Circle();
+            }
+            return null;
+        }
+    }
+}
