1f029b349b2737099e9ec61b10211c6f81a1a0c3
Hemant Patidar <patidarhemant27@gmail.com>
2023-06-03 23:58:23 +0530

    Abstract Factory Design sample and tried for class diagram of the same

---
+++ Manifestations/Manifestations/Creational/AbstractFactoryDesign.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manifestations.Creational
+{
+    // Factory of Factory
+    internal class AbstractFactoryDesign
+    {
+    }
+
+    public interface ICar
+    {
+        public int getTopSpeed();
+    }
+    public interface IAbstractFactory
+    {
+        ICar getInstance(int price);
+    }
+
+    public class EconomicCarFactory : IAbstractFactory
+    {
+        public ICar getInstance(int price)
+        {
+            if(price < 300000)
+            {
+                return new EconmicCar1();
+            }
+            else if (price > 300000)
+            {
+                return new EconmicCar2();
+            }
+            return null;
+        }
+    }
+
+    public class LuxaryClassFactory : IAbstractFactory
+    {
+        public ICar getInstance(int price)
+        {
+            if(price >= 1000000 && price <= 2000000)
+            {
+                return new LuxeryCar1();
+            }
+            else if(price > 2000000)
+            {
+                return new LuxeryCar2();
+            }
+            return null;
+        }
+    }
+
+    public class AbstractFactoryProducer
+    {
+        public IAbstractFactory getFactoryInstance(string value)
+        {
+            if (value.Equals("Economic"))
+            {
+                return new EconomicCarFactory();
+            }
+            else if(value.Equals("Luxary") || value.Equals("Premium"))
+            {
+                return new LuxaryClassFactory();
+            }
+            return null;
+        }
+    }
+}
---
+++ Manifestations/Manifestations/Creational/EconmicCar1.cs
@@ -0,0 +1,34 @@
+﻿namespace Manifestations.Creational
+{
+    internal class EconmicCar1 : ICar
+    {
+        public int getTopSpeed()
+        {
+            return 60;
+        }
+    }
+
+    internal class EconmicCar2 : ICar
+    {
+        public int getTopSpeed()
+        {
+            return 100;
+        }
+    }
+
+    internal class LuxeryCar1 : ICar
+    {
+        public int getTopSpeed()
+        {
+            return 200;
+        }
+    }
+
+    internal class LuxeryCar2 : ICar
+    {
+        public int getTopSpeed()
+        {
+            return 250;
+        }
+    }
+}
